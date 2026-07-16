c6c3b5913ea1f8572a535c78798f90847093ceb9
Administrador <admin@gfm.com>
2026-07-16 18:46:45 -0300

    test: plain text handler

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 exports.handler = async (req, res) => {
-  res.writeHead(500, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    message: 'test handler',
-    method: req.method,
-    url: req.url,
-    env: process.env.DATABASE_URL ? 'DB set' : 'DB not set',
-  }));
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from Vercel! ' + req.method + ' ' + req.url);
 };
