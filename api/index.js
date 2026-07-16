66ed4b29408bff19122051dfffbaa25ac229ab5c
Administrador <admin@gfm.com>
2026-07-16 18:49:55 -0300

    fix: set outputDirectory to . and simplify handler

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 exports.handler = async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from Vercel! ' + req.method + ' ' + req.url);
+  res.end('OK');
 };
